ba6c2d11d2fc001c7e2bb07d9e6846ec362e9b40
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2021-01-21 09:56:48 -0600

    Specify ipa path to resign command

--- resign.sh
+++ resign.sh
@@ -18,7 +18,7 @@ security unlock-keychain -p "$KEYCHAIN_PASS" "$KEYCHAIN_PATH"
 security import cicert.p12 -P "$P12_PASS" -A -t cert -f pkcs12 -k "$KEYCHAIN_PATH"
 security list-keychain -d user -s "$KEYCHAIN_PATH"
 
-fastlane sigh resign --signing_identity "$SIGNING_IDENTITY" -p "CI.mobileprovision"
+fastlane sigh resign "$IPA_PATH" --keychain_path "$KEYCHAIN_PATH" --signing_identity "$SIGNING_IDENTITY" --provisioning_profile "CI.mobileprovision"
 
 # Clean up
 rm ~/Library/MobileDevice/Provisioning\ Profiles/CI.mobileprovision
