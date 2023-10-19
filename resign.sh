d3c2e4cb930bc7edf89894af7af622d3c0d5b571
Marco Maetzler <105300787+mmaetzler@users.noreply.github.com>
2023-10-18 17:37:09 -0700

    fix: update how data is decoded

--- resign.sh
+++ resign.sh
@@ -4,14 +4,14 @@ set -e
 
 # Regenerate the mobileprovision from base64
 cd "$RUNNER_TEMP" || exit 1
-echo -n "$MOBILEPROVISION" | base64 --decode --output CI.mobileprovision
+echo -n "$MOBILEPROVISION" | base64 --decode > CI.mobileprovision
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp CI.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
 
 # Regenerate the p12 from base64 and install in new temp keychain
 KEYCHAIN_PATH=$RUNNER_TEMP/temp_resign
 KEYCHAIN_PASS=$(echo "$(date)""$RANDOM" | base64)
-echo -n "$CERT_P12" | base64 --decode --output cicert.p12
+echo -n "$CERT_P12" | base64 --decode > cicert.p12
 security create-keychain -p "$KEYCHAIN_PASS" "$KEYCHAIN_PATH"
 security set-keychain-settings -lut 21600 "$KEYCHAIN_PATH"
 security unlock-keychain -p "$KEYCHAIN_PASS" "$KEYCHAIN_PATH"
